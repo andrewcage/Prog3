821db1e75d267f60195332f88b31539e697972e3
user <user@lab0000m.tumo.lab>
2019-06-11 18:21:29 +0400

    Տո անասուուուուուն

---
+++ LiqyMacunovGreshka/server.js
@@ -0,0 +1,120 @@
+
+//! Requiring modules  --  START
+var Grass = require("./modules/Grass.js");
+var GrassEater = require("./modules/GrassEater.js");
+var Predator = require("./modules/Predator.js")
+var Smuggler = require("./modules/Smuggler.js")
+let random = require("./modules/random.js");
+//! Requiring modules  --  END
+
+
+//! Setting global arrays  --  START
+grassArr = [];
+grassEaterArr = [];
+predArr = []
+smugArr = []
+matrix = [];
+grassHashiv = 0;
+//! Setting global arrays  -- END
+
+
+
+
+//! Creating MATRIX -- START
+function matrixGenerator(matrixSize, grass, grassEater, grassEaterEater, predArr, smugArr) {
+    for (let i = 0; i < matrixSize; i++) {
+        matrix[i] = [];
+        for (let o = 0; o < matrixSize; o++) {
+            matrix[i][o] = 0;
+        }
+    }
+    for (let i = 0; i < grass; i++) {
+        let customX = Math.floor(random(matrixSize)); // 0-9
+        let customY = Math.floor(random(matrixSize)); // 4
+        matrix[customY][customX] = 1;
+    }
+    for (let i = 0; i < grassEater; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 2;
+    }
+    for (let i = 0; i < pred; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 3;
+    }
+    for (let i = 0; i < smug; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 4;
+    }
+}
+matrixGenerator(20, 1, 1);
+//! Creating MATRIX -- END
+
+
+
+//! SERVER STUFF  --  START
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+app.use(express.static("."));
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+server.listen(3000);
+//! SERVER STUFF END  --  END
+
+
+
+function creatingObjects() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 2) {
+                var grassEater = new GrassEater(x, y);
+                grassEaterArr.push(grassEater);
+            }
+            else if (matrix[y][x] == 3) {
+                var pred = new Predator(x, y);
+                predArr.push(pred);
+            } 
+            else if (matrix[y][x] == 4) {
+                var smug = new Smuggler(x, y);
+                smugArr.push(smug);
+            } 
+            else if (matrix[y][x] == 1) {
+                var grass = new Grass(x, y);
+                grassArr.push(grass);
+                grassHashiv++;
+            }
+
+        }
+    }
+    creatingObjects();
+
+    function game() {
+        if (grassArr[0] !== undefined) {
+            for (var i in grassArr) {
+                grassArr[i].mul();
+            }
+        }
+        if (grassEaterArr[0] !== undefined) {
+            for (var i in grassEaterArr) {
+                grassEaterArr[i].eat();
+            }
+        }
+
+        //! Object to send
+        let sendData = {
+            matrix: matrix,
+            grassCounter: grassHashiv
+        }
+
+        //! Send data over the socket to clients who listens "data"
+        io.sockets.emit("data", sendData);
+    }
+
+
+
+    setInterval(game, 1000)
---
+++ Proj/Smuggler.js
@@ -0,0 +1,140 @@
+var LivingCreature = require("./LivingCreature.js")
+var random = require("./random.js")
+
+module.export = class Smuggler extends LivingCreature {
+    constructor(x, y, index) {
+        super(x, y, index)
+        this.energy = 15
+    }
+    getNewCoordinatesShoot() {
+        this.directionsShoot = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y - 1],
+            [this.x + 2, this.y - 1],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 1],
+            [this.x + 2, this.y + 1],
+            [this.x - 2, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x + 2, this.y + 2]
+        ];
+    }
+
+    getNewCoordinatesMove() {
+        this.directionsMove = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCellShoot(character) {
+        this.getNewCoordinatesShoot();
+        return super.chooseCell(character);
+    }
+
+    chooseCellMove(character) {
+        this.getNewCoordinatesMove();
+        return super.chooseCell(character);
+    }
+
+
+    move() {
+
+        var newCell = random(this.chooseCellMove(0));
+        var newCell2 = random(this.chooseCellMove(1));
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[this.y][this.x] = 0;
+            matrix[newY][newX] = this.index;
+
+
+            this.y = newY;
+            this.x = newX;
+        }
+        else if (newCell2) {
+            var newX2 = newCell2[0];
+            var newY2 = newCell2[1];
+
+
+            matrix[this.y][this.x] = 1;
+            matrix[newY2][newX2] = this.index;
+
+            this.y = newY2;
+            this.x = newX2;
+        }
+
+        this.energy -= 2;
+
+    }
+
+
+    shoot() {
+
+        var newCell = this.chooseCellShoot(2);
+        var newCell2 = this.chooseCellShoot(3);
+        var newCell3 = newCell.concat(newCell2)
+        var newCell = random(newCell3)
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[this.y][this.x] = this.index;
+            matrix[newY][newX] = 0;
+
+
+            for (var i in grassEaterArr) {
+                if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                    grassEaterArr.splice(i, 1)
+                    break;
+                }
+            }
+
+            for (var i in predatorArr) {
+                if (newX == predatorArr[i].x && newY == predatorArr[i].y) {
+                    predatorArr.splice(i, 1)
+                    break;
+                }
+            }
+            this.energy += 2;
+        }
+    }
+
+    leave() {
+
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0;
+            for (var i in smugglerArr) {
+                if (this.x == smugglerArr[i].x && this.y == smugglerArr[i].y) {
+                    smugglerArr.splice(i, 1)
+                }
+            }
+        }
+    }
+}
+
+
